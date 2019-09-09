c1a4bb84f44ad7f6c80c104d6173ae6f7300e70c
Mihkel Vaher <mihkelva@users.noreply.github.com>
2019-09-09 11:20:39 +0300

    õppetöö jaoks failistruktuur loodud

---
+++ 02tund/try.php
@@ -0,0 +1,37 @@
+<?php
+  $userName = "Mihkel Vaher";
+  $fullTimeNow = date("d.m.Y H:i:s");
+  $hourNow = date("H");
+  $partOfDay = "hägune aeg";
+  if($hourNow < 8){
+	$partOfDay = "varane hommik";
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="et">
+<head>
+  <meta charset="utf-8">
+  <title>Üliõpilase 
+  <?php 
+  echo $userName;
+  ?> 
+  veeb</title>
+</head>
+<body>
+  <?php
+    echo "<h1>" .$userName ." elab siin lehel</h1>"
+  ?>
+  <p>See leht on loodud koolis õppetöö raames ja ei sisalda tõsiseltvõetavat sisu!!!!!!</p>
+  <hr>
+  <p> Lehe avamise hetkel oli aeg: 
+   <?php 
+  echo $fullTimeNow;
+  ?>
+  .</p>
+  <?php 
+  echo "<p> Lehe avamisel hetkel oli " .$partOfDay .".</p>";
+  ?>
+
+</body>
+</html>
